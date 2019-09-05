b375a841908369ff856de377e323758a74080481
katerine Muñoz <katerine.munoz02@gmail.com>
2019-09-04 20:59:00 -0500

    septiembre

--- src/main/java/com/accenture/rentacar/app/controller/VehiculoController.java
+++ src/main/java/com/accenture/rentacar/app/controller/VehiculoController.java
@@ -3,7 +3,11 @@ package com.accenture.rentacar.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +28,42 @@ public class VehiculoController {
 		return vehiculoService.listarTodos();
 	}
 	
-
+	@GetMapping("/vehiculos/{id}")
+	public Vehiculo buscarPorID(@PathVariable Long id) {
+		
+		return vehiculoService.buscarVehiculoPorId(id);
+	}
+	
+	@PostMapping("/vehiculos")
+	public Vehiculo guardar (@RequestBody Vehiculo vehiculo) {
+		return vehiculoService.guardar(vehiculo);
+	}
+	
+	@SuppressWarnings ("unused")
+	@PostMapping("/actualizar")
+	public Vehiculo actualizar (@RequestBody Vehiculo vehiculo) {
+		
+		Vehiculo vehiAActualizar = new Vehiculo ("Toyota"));
+		Vehiculo vehiActual = vehiculoService.buscarVehiculoPorId(vehiculo.getId());
+		vehiAActualizar.setId(vehiculo.getId());
+		vehiAActualizar.setColor(vehiculo.getColor());
+		vehiAActualizar.setLinea(vehiculo.getLinea());
+		vehiAActualizar.setMarca(vehiculo.getMarca());
+		vehiAActualizar.setModeloAuto(vehiculo.getModeloAuto());
+		vehiAActualizar.setPlaca(vehiculo.getPlaca());
+		
+		return vehiculoService.guardar(vehiAActualizar);
+	}
+	
+	@DeleteMapping("/vehiculos/{id}")
+	public String borrar (@PathVariable Long id) {
+		vehiculoService.borrar(id);
+		return "El vehiculo se eliminó correctamente";
+	}
+	  
+	
 }
+	
+	
+	
+
